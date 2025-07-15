903e891b030cbd9a3896b06cb4367ad816c36566
bhavin6305 <bhavinrajput2005@gmail.com>
2025-07-15 18:10:50 +0530

    Project Started

---
+++ Frontend/src/components/ui/custom/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <div className='p-2 shadow-sm'/>
+
+      <img src="/logo.svg" alt="" />
+      <div>
+        <button>Sign In</button>
+      </div>
+    </div>
+  )
+}
